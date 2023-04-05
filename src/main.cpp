66dd599fe8dc2a935edd27c586a1c2e50b59dacd
jstri114 <jstri114@students.kennesaw.edu>
2023-04-05 02:59:28 -0400

    Cleaned up some more

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,12 @@
 #include <FreqMeasureMulti.h>
 #include <MDB_labels.h>
 
-enum ACUSTATE
-{
-  BOOTUP,
-  RUNNING,
-  FAULT
-};
-int acuState = 0;
-#define runningFoReal
 #define DEBUG true
 
-// BIG DEFINES FOR MODULES (absolute cancer code)
+// BIG DEFINES FOR MODULES & SWITCH CASE (absolute cancer code)
 #define NUMBER_OF_CELLS 72
 #define NUMBER_OF_MODULES 6
-#define CELLS_PER_MODULE (NUMBER_OF_CELLS/NUMBER_OF_MODULES)
+#define CELLS_PER_MODULE 12
 
 #define CELLS_1A CELLS_PER_MODULE*0/2
 #define CELLS_1B CELLS_PER_MODULE*1/2
@@ -35,35 +27,11 @@ int acuState = 0;
 
 int8_t batteryTemps[NUMBER_OF_CELLS];
 
-#ifdef runningFoReal
-#define NUMBER_OF_LTCs 5
-
-uint8_t gettingTempState = 0; // 0=set 1=wait 2=get
-// Init ADCs
-// Ltc2499 theThings[6];
-float batteryTempvoltages[NUMBER_OF_CELLS];
-// uint8_t ltcAddressList[] = {ADDR_Z00, ADDR_Z0Z, ADDR_0Z0, // one two three
-//                             ADDR_ZZ0, ADDR_0ZZ};          // first 6 configurable addresses in the mf datasheet
-// byte ADCChannels[] = {CHAN_SINGLE_0P, CHAN_SINGLE_1P, CHAN_SINGLE_2P, CHAN_SINGLE_3P,
-//                       CHAN_SINGLE_4P, CHAN_SINGLE_5P, CHAN_SINGLE_6P, CHAN_SINGLE_7P,
-//                       CHAN_SINGLE_8P, CHAN_SINGLE_9P, CHAN_SINGLE_10P, CHAN_SINGLE_11P};
-elapsedMillis conversionTime; // wait 80ms for conversion to be ready
-#endif
-
-// ACC Canbus stuff
+// Canbus stuff
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> CAN_1;
-FlexCAN_T4 <CAN2, RX_SIZE_256, TX_SIZE_16> ACU_;
+FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> ACC_1;
 #define NUM_TX_MAILBOXES 2
 #define NUM_RX_MAILBOXES 6
-CAN_message_t rxMsg;
-uint8_t temp1[8];
-uint8_t temp2[8];
-
-
-
-int currentChannel = 0;
-int globalHighTherm = 25, globalLowTherm = 25;
-int HBstate = 0;
 
 // Can IDs
 #define BMS_ID 0x7E3
@@ -72,6 +40,7 @@ int HBstate = 0;
 #define BMS_Response_ID 0x7EB
 
 // Can bytes
+CAN_message_t rxMsg;
 int moduleNo = 0;                                                         // byte0
 int enabledTherm;                                                         // byte4
 byte getLowestTemp[] = {0x03, 0x22, 0xF0, 0x28, 0x55, 0x55, 0x55, 0x55};  // lowest temp request
@@ -79,32 +48,25 @@ byte getHighestTemp[] = {0x03, 0x22, 0xF0, 0x29, 0x55, 0x55, 0x55, 0x55}; // low
 Metro sendTempRate = Metro(100);
 Metro getTempRate = Metro(500);
 Metro doThingsRate = Metro(100);
-Metro heartbeat = Metro(100);
+Metro heartBeat = Metro(100);
 
+// Globals
+int globalHighTherm = 25, globalLowTherm = 25;
+int heartBeatState = 0;
 
-
-// printing received to serial
-void canSniff(const CAN_message_t &msg);
+// Function defs
+int ReadACC_1(CAN_message_t &msg);
+void updateAccumulatorCAN();
 void getTempData();
 void sendTempData();
-void ACUStateMachine();
-int setChannels(int channelNo);
-void setChannelsSwitchCase(int channelNo);
-void getTemps(int channelNo);
-void DebuggingPrintout();
 
-int ReadBatteryTemps(CAN_message_t &msg);
-void updateAccumulatorCAN();
-void init_ACU_CAN();
-
+// Setup -----------------------------------------------------------------------
 void setup()
 {
-  Wire.setClock(100000);
-  Wire.begin();
   Serial.begin(115200);
   delay(400);
 
-  // This is temp array
+  // This is the main battery temp array
   Serial.println("Battery temp array: ");
   for (int i = 0; i < NUMBER_OF_CELLS; i++)
   {
@@ -115,28 +77,38 @@ void setup()
     Serial.println(batteryTemps[i]);
   }
 
+  // CAN 1 setup
   CAN_1.begin();
   CAN_1.setBaudRate(500000);
   CAN_1.setMaxMB(16);
   CAN_1.enableFIFO();
   CAN_1.enableFIFOInterrupt();
-#ifdef DEBUG
-  CAN_1.onReceive(canSniff);
-#endif
   CAN_1.mailboxStatus();
   // Serial.println("Send something on serial to continue...");
   // while(!Serial.available());{ }
   
-  init_ACU_CAN();
+  // Isolated ACC CAN setup
+  ACC_1.begin();
+  ACC_1.setBaudRate(500000);
+  ACC_1.setMaxMB(NUM_TX_MAILBOXES + NUM_RX_MAILBOXES);
+  for (int i = 0; i < NUM_RX_MAILBOXES; i++)
+  {
+    ACC_1.setMB((FLEXCAN_MAILBOX)i, RX, STD);   
+  }
+  for (int i = NUM_RX_MAILBOXES; i < (NUM_TX_MAILBOXES + NUM_RX_MAILBOXES); i++)
+  {
+    ACC_1.setMB((FLEXCAN_MAILBOX)i, TX, STD);
+  }
 }
+// Setup -----------------------------------------------------------------------
 
 // Main loop -----------------------------------------------------------------------
 void loop()
 {
-  if(heartbeat.check())
+  if(heartBeat.check())
   {
-    HBstate=!HBstate;
-    digitalWrite(LED_BUILTIN, HBstate);
+    heartBeatState=!heartBeatState;
+    digitalWrite(LED_BUILTIN, heartBeatState);
   }
   
   CAN_1.events();
@@ -148,38 +120,15 @@ void loop()
   if (sendTempRate.check() == 1)
   {
     sendTempData();
-    #if DEBUG
-    // DebuggingPrintout();
-    #endif
   }
 }
 // Main loop -----------------------------------------------------------------------
 
-void init_ACU_CAN()
-{
-    ACU_.begin();
-    ACU_.setBaudRate(500000);
-    ACU_.setMaxMB(NUM_TX_MAILBOXES + NUM_RX_MAILBOXES);
-    for (int i = 0; i < NUM_RX_MAILBOXES; i++)
-    {
-      ACU_.setMB((FLEXCAN_MAILBOX)i, RX, STD);   
-    }
-    for (int i = NUM_RX_MAILBOXES; i < (NUM_TX_MAILBOXES + NUM_RX_MAILBOXES); i++)
-    {
-      ACU_.setMB((FLEXCAN_MAILBOX)i, TX, STD);
-    }
-}
-
-int ReadBatteryTemps(CAN_message_t &msg)
-{    
-  int rxMSG = ACU_.read(msg);
-  return rxMSG;
-}
-
 void updateAccumulatorCAN()
 {  
-  if (ReadBatteryTemps(rxMsg))
+  if (ReadACC_1(rxMsg))
   {
+    #ifdef DEBUG
     Serial.print("MB "); Serial.print(rxMsg.mb);
     Serial.print("  OVERRUN: "); Serial.print(rxMsg.flags.overrun);
     Serial.print("  LEN: "); Serial.print(rxMsg.len);
@@ -191,56 +140,95 @@ void updateAccumulatorCAN()
       Serial.print(rxMsg.buf[i], HEX); Serial.print(" ");
     } 
     Serial.println();
-    switch (rxMsg.id)  // This is cancer and could better be implemented with a loop I imagine
+    #endif
+
+    switch (rxMsg.id)  // This is cancer probably and could better be implemented with a loop I imagine
     {
-    case (MODULE_1_A): 
-    {
-      Serial.println("Module 1 A");
-      memcpy(batteryTemps+CELLS_1A,rxMsg.buf,6);
-      break;
-    }
-    case (MODULE_1_B): 
-    {
-      Serial.println("Module 1 B");
-      memcpy(batteryTemps+CELLS_1B,rxMsg.buf,6);
-      break;
-    }
-    default:
-      break;
+      case (MODULE_1_A): 
+      {
+        Serial.println("Module 1 A");
+        memcpy(batteryTemps+CELLS_1A,rxMsg.buf,6);
+        break;
+      }
+      case (MODULE_1_B): 
+      {
+        Serial.println("Module 1 B");
+        memcpy(batteryTemps+CELLS_1B,rxMsg.buf,6);
+        break;
+      }
+      case (MODULE_2_A): 
+      {
+        Serial.println("Module 2 A");
+        memcpy(batteryTemps+CELLS_2A,rxMsg.buf,6);
+        break;
+      }
+      case (MODULE_2_B): 
+      {
+        Serial.println("Module 2 B");
+        memcpy(batteryTemps+CELLS_2B,rxMsg.buf,6);
+        break;
+      }
+      case (MODULE_3_A): 
+      {
+        Serial.println("Module 3 A");
+        memcpy(batteryTemps+CELLS_3A,rxMsg.buf,6);
+        break;
+      }
+      case (MODULE_3_B): 
+      {
+        Serial.println("Module 3 B");
+        memcpy(batteryTemps+CELLS_3B,rxMsg.buf,6);
+        break;
+      }
+      case (MODULE_4_A): 
+      {
+        Serial.println("Module 4 A");
+        memcpy(batteryTemps+CELLS_4A,rxMsg.buf,6);
+        break;
+      }
+      case (MODULE_4_B): 
+      {
+        Serial.println("Module 4 B");
+        memcpy(batteryTemps+CELLS_4B,rxMsg.buf,6);
+        break;
+      }
+      case (MODULE_5_A): 
+      {
+        Serial.println("Module 5 A");
+        memcpy(batteryTemps+CELLS_5A,rxMsg.buf,6);
+        break;
+      }
+      case (MODULE_5_B): 
+      {
+        Serial.println("Module 5 B");
+        memcpy(batteryTemps+CELLS_5B,rxMsg.buf,6);
+        break;
+      }
+      case (MODULE_6_A): 
+      {
+        Serial.println("Module 6 A");
+        memcpy(batteryTemps+CELLS_6A,rxMsg.buf,6);
+        break;
+      }
+      case (MODULE_6_B): 
+      {
+        Serial.println("Module 6 B");
+        memcpy(batteryTemps+CELLS_6B,rxMsg.buf,6);
+        break;
+      }
+      default:
+      {
+        break;
+      }
     }
   }
 }
 
-void canSniff(const CAN_message_t &msg)
-{
-  if(msg.id==BMS_Response_ID){
-    Serial.print("MB "); Serial.print(msg.mb);
-    Serial.print("  OVERRUN: "); Serial.print(msg.flags.overrun);
-    Serial.print("  LEN: "); Serial.print(msg.len);
-    Serial.print(" EXT: "); Serial.print(msg.flags.extended);
-    Serial.print(" TS: "); Serial.print(msg.timestamp);
-    Serial.print(" ID: "); Serial.print(msg.id, HEX);
-    Serial.print(" Buffer: ");
-    for ( uint8_t i = 0; i < msg.len; i++ ) {
-      Serial.print(msg.buf[i], HEX); Serial.print(" ");
-    } 
-    Serial.println();
-  }
-}
-
-// getting one of the max temps from BMS (high or low not sure lol)
-void getTempData()
-{
-  CAN_message_t getTempMsg;
-  getTempMsg.flags.extended = 1;
-  getTempMsg.len = 8;
-  getTempMsg.id = BMS_ID; // OUR BMS
-  memcpy(getTempMsg.buf, getLowestTemp, sizeof(getTempMsg.buf));
-  CAN_1.write(getTempMsg);
-  Serial.println("Requesting Lowest Temp Data...");
-  memcpy(getTempMsg.buf, getHighestTemp, sizeof(getTempMsg.buf));
-  CAN_1.write(getTempMsg);
-  Serial.println("Requesting Highest Temp Data...");
+// Read the ACC can
+int ReadACC_1(CAN_message_t &msg)
+{    
+  int rxMSG = ACC_1.read(msg);
+  return rxMSG;
 }
 
 // Sending highest/lowest temperature to the BMS
@@ -251,14 +239,17 @@ void sendTempData()
   sendTempMsg.len = 8;                 // per protocol
   sendTempMsg.id = ThermistorToBMS_ID; // Temp broadcast ID
   enabledTherm = NUMBER_OF_CELLS - 1;  // number of cells 0 based
-  int lowTherm = batteryTemps[0], lowestThermId, highTherm = batteryTemps[0], highestThermId;
+  int lowTherm = batteryTemps[0];
+  int lowestThermId = 0;
+  int highTherm = batteryTemps[0];
+  int highestThermId = 0;
   for (int i = 0; i < NUMBER_OF_CELLS; i++)
   { // get lowest and highest
     #ifdef DEBUG
-    Serial.print("Cell number: ");
-    Serial.print(i);
-    Serial.print(" Value: ");
-    Serial.println(batteryTemps[i]);
+    // Serial.print("Cell number: ");
+    // Serial.print(i);
+    // Serial.print(" Value: ");
+    // Serial.println(batteryTemps[i]);
     #endif
     if (batteryTemps[i] < lowTherm)
     {
@@ -278,6 +269,9 @@ void sendTempData()
   int avgTherm = (lowTherm + highTherm) / 2;                                                                          // yep
   int checksum = moduleNo + lowTherm + highTherm + avgTherm + enabledTherm + highestThermId + lowestThermId + 57 + 8; // 0x39 and 0x08 added to checksum per orion protocol
   byte tempdata[] = {moduleNo, lowTherm, highTherm, avgTherm, enabledTherm, highestThermId, lowestThermId, checksum};
+  #ifdef DEBUG
+  // Serial.println(tempdata[2]);
+  #endif
   memcpy(sendTempMsg.buf, tempdata, sizeof(sendTempMsg.buf));
   CAN_1.write(sendTempMsg);
   // GLobal ints for tracking
@@ -285,220 +279,17 @@ void sendTempData()
   globalLowTherm = lowTherm;
 }
 
-// int setChannels(int channelNo)
-// {
-//   for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//   {
-//     theThings[i].changeChannel(ADCChannels[channelNo]);
-//     return channelNo;
-//   }
-// }
-
-// void setChannelsSwitchCase(int channelNo)
-// {
-//   switch (channelNo)
-//   {
-//   case 0:
-//   {
-//     for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//     {
-//       theThings[i].changeChannel(CHAN_SINGLE_0P);
-//     }
-//   }
-//   break;
-//   case 1:
-//   {
-//     for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//     {
-//       theThings[i].changeChannel(CHAN_SINGLE_1P);
-//       break;
-//     }
-//   }
-//   break;
-//   case 2:
-//   {
-//     for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//     {
-//       theThings[i].changeChannel(CHAN_SINGLE_2P);
-//       break;
-//     }
-//   }
-//   break;
-//   case 3:
-//   {
-//     for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//     {
-//       theThings[i].changeChannel(CHAN_SINGLE_3P);
-//     }
-//   }
-//   break;
-//   case 4:
-//   {
-//     for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//     {
-//       theThings[i].changeChannel(CHAN_SINGLE_4P);
-//     }
-//   }
-//   break;
-//   case 5:
-//   {
-//     for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//     {
-//       theThings[i].changeChannel(CHAN_SINGLE_5P);
-//     }
-//   }
-//   break;
-//   case 6:
-//   {
-//     for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//     {
-//       theThings[i].changeChannel(CHAN_SINGLE_6P);
-//       break;
-//     }
-//   }
-//   break;
-//   case 7:
-//   {
-//     for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//     {
-//       theThings[i].changeChannel(CHAN_SINGLE_7P);
-//     }
-//   }
-//   break;
-//   case 8:
-//   {
-//     for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//     {
-//       theThings[i].changeChannel(CHAN_SINGLE_8P);
-//     }
-//   }
-//   break;
-//   case 9:
-//   {
-//     for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//     {
-//       theThings[i].changeChannel(CHAN_SINGLE_9P);
-//     }
-//   }
-//   break;
-//   case 10:
-//   {
-//     for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//     {
-//       theThings[i].changeChannel(CHAN_SINGLE_10P);
-//     }
-//   }
-//   break;
-//   case 11:
-//   {
-//     for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//     {
-//       theThings[i].changeChannel(CHAN_SINGLE_11P);
-//     }
-//   }
-//   break;
-//   }
-// }
-
-// void getTemps(int channelNo)
-// {
-//   CAN_message_t energus_voltages;
-//   energus_voltages.id = 0x6B3;
-//   energus_voltages.buf[0] = channelNo;
-//   energus_voltages.len = 8;
-//   for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//   {
-
-//     float v = theThings[i].readVoltage();
-//     int cellNum = (i * 12) + channelNo;
-//     batteryTempvoltages[cellNum] = v;
-//     float vv = v * 100;
-//     energus_voltages.buf[i + 1] = (int)vv;
-//     Serial.printf("Can Message Test: %d\n", energus_voltages.buf[i + 1]);
-//     float temp = (v * -79.256) + 168.4;
-//     batteryTemps[cellNum] = temp;
-// #ifdef DEBUG
-//     char buffer[100];
-//     sprintf(buffer, "LTC Number: %d CellNum: %d Channel: %d Reading: %f TempC: %f ", i, cellNum, channelNo, v, temp);
-//     Serial.println(buffer);
-// #endif
-//   }
-//   CAN_1.write(energus_voltages);
-// }
-
-// #ifdef runningFoReal
-// void getAllTheTemps(int channelNo)
-// {
-//   for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//   {
-//     theThings[i].changeChannel(CHAN_SINGLE_0P);
-//   }
-//   conversionTime = 0;
-//   delay(100);
-//   for (int i = 0; i < NUMBER_OF_LTCs; i++)
-//   {
-//     float voltageX = theThings[i].readVoltage();
-//     Serial.println(voltageX);
-//     int readingNumber = (i * 12) + channelNo;
-//     batteryTempvoltages[readingNumber] = voltageX;
-// #ifdef DEBUG
-//     char buffer[50];
-//     sprintf(buffer, "LTC Number: %d  Channel: %d Voltage: %f Reading Number: %d", i, channelNo, voltageX, readingNumber);
-//     Serial.println(buffer);
-// #endif
-//   }
-//   // Serial.println("=================");
-// }
-// #endif
-
-
-// // void ACUStateMachine()
-// {
-//   // Serial.println("State:");
-//   // Serial.println(acuState);
-//   switch (acuState)
-//   {
-//   case 0:
-//   {
-//     setChannelsSwitchCase(currentChannel);
-//     conversionTime = 0;
-//     acuState = 1;
-//     Serial.println("Setting Channels: ");
-//     break;
-//   }
-//   case 1:
-//   {
-//     if (conversionTime >= 100)
-//     {
-//       acuState = 2;
-//       Serial.println("Going to get readings");
-//     }
-//     break;
-//   }
-//   case 2:
-//   {
-//     Serial.println("reading channels");
-//     getTemps(currentChannel);
-//     acuState = 0;
-//     currentChannel++;
-//     if (currentChannel > 11)
-//     {
-//       currentChannel = 0;
-//     }
-//     break;
-//   }
-//   }
-// }
-
-void DebuggingPrintout()
+// getting one of the max temps from BMS (high or low not sure lol)
+void getTempData()
 {
-  for (int i = 0; i < NUMBER_OF_CELLS; i++)
-  {
-    Serial.print("Cell Number: ");
-    Serial.print(i + 1);
-    Serial.print(" Temp: ");
-    Serial.print(batteryTemps[i]);
-    Serial.println();
-  }
+  CAN_message_t getTempMsg;
+  getTempMsg.flags.extended = 1;
+  getTempMsg.len = 8;
+  getTempMsg.id = BMS_ID; // OUR BMS
+  memcpy(getTempMsg.buf, getLowestTemp, sizeof(getTempMsg.buf));
+  CAN_1.write(getTempMsg);
+  Serial.println("Requesting Lowest Temp Data...");
+  memcpy(getTempMsg.buf, getHighestTemp, sizeof(getTempMsg.buf));
+  CAN_1.write(getTempMsg);
+  Serial.println("Requesting Highest Temp Data...");
 }
-
